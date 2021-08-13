6d647b37e9d148b5a76af40f393905f22f31ee31
GusNitrous <gusnitrous@gmail.com>
2021-08-13 22:43:30 +0300

    Feature: using HTTP CORS with default options

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const corsOptions = {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+    };
+
+    const app = await NestFactory.create(AppModule, { cors: corsOptions });
     const config = app.get(ConfigService);
     await app.listen(config.get<number>("APP_LISTEN_PORT"));
 }
