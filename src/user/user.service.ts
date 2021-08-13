d6633efc274295bd7ee2092110bd248911172422
GusNitrous <gusnitrous@gmail.com>
2021-08-13 22:37:41 +0300

    Feature: findById in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from "@nestjs/common";
-import { UserEntity } from "./persistence/user.entity";
+import { User, UserId } from "./persistence/user.entity";
 import { UserRepository } from "./persistence/user.repository";
 
 @Injectable()
 export class UserService {
     constructor(private readonly userRepository: UserRepository) {}
 
-    create(data: Partial<UserEntity>): Promise<UserEntity> {
-        return Promise.resolve(this.userRepository.save(UserEntity.from(data)));
+    create(data: Partial<User>): Promise<User> {
+        return Promise.resolve(this.userRepository.save(User.from(data)));
+    }
+
+    findById(userId: UserId): Promise<User> {
+        return Promise.resolve(this.userRepository.findById(userId));
     }
 }
