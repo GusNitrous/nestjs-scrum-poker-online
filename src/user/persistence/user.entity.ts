dcb77b7f4dcf769ffac13f87c5c10d5decb046d0
GusNitrous <gusnitrous@gmail.com>
2021-08-13 22:36:43 +0300

    Refactoring: renamed UserEntity; room id's and socket id's in User

--- src/user/persistence/user.entity.ts
+++ src/user/persistence/user.entity.ts
@@ -1,9 +1,11 @@
 import { UserRole } from "../constants/user-role";
 import { v4 as uuidv4 } from "uuid";
 
-export class UserEntity {
-    public static from(data: Partial<UserEntity>): UserEntity {
-        const newUser = new UserEntity();
+export type UserId = string;
+
+export class User {
+    public static from(data: Partial<User>): User {
+        const newUser = new User();
         newUser.name = data.name;
         newUser.role = data.role;
         newUser.createdAt = new Date();
@@ -18,13 +20,23 @@ export class UserEntity {
 
     public updatedAt: Date;
 
-    private readonly _id: string = uuidv4();
+    public createdRoomIds: Set<string>;
+
+    public socketIds: Set<string>;
+
+    private readonly _id: UserId = uuidv4();
+
+    constructor() {
+        this.createdAt = new Date();
+        this.createdRoomIds = new Set();
+        this.socketIds = new Set();
+    }
 
     get id(): string {
         return this._id;
     }
 
-    pupulate(data: Partial<UserEntity>): this {
+    pupulate(data: Partial<User>): this {
         this.name = data.name;
         this.role = data.role;
         this.updatedAt = new Date();
