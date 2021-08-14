457f7e48b9ba419dd676c13840590da0102c9421
GusNitrous <gusnitrous@gmail.com>
2021-08-14 12:55:13 +0300

    Feature: client register, creating room, join to room; basic exception handling

--- client/index.js
+++ client/index.js
@@ -1,103 +1,95 @@
-document.addEventListener('DOMContentLoaded', async () => {
-
 const BASE_URL = 'http://localhost:3000';
+let inputRegister = null;
+let inputRoomId = null;
+let socket = null;
 
-let userId = null;
-
-const inputRegister = document.getElementById('inputRegister');
-const inputRoomId = document.getElementById('inputRoomId');
-
-
-
-const btnRegister = document.getElementById('btnRegister');
-
-const btnCreate = document.getElementById('btnCreateRoom');
-const btnConnect = document.getElementById('btnConnectRoom');
-
-
-const socket = io(BASE_URL, {
-	auth: {
-		token: jwtToken
-	}
-});
-
-async function register() {
+function register() {
 	const userName = inputRegister.value;
-	const response = await fetch(`${BASE_URL}/auth/register`, {method: 'POST', body: {userName}});
-	if (!response.ok) {
-		const authData = await response.json();
-		saveAuthData(authData);
-	} else {
-		alert('Ошибка регистрации!');
-		console.log(response);
-	}
+	const requestData = {userName};
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(requestData) 
+	};
+
+	fetch(`${BASE_URL}/auth/register`, options).then(async (response) => {
+		const result = await response.json();
+		if (response.ok) {
+			saveAuthData(result);
+			console.log('auth_data', result);
+			socket = initSocket(result);
+		} else {
+			alert('Ошибка регистрации!');
+			console.log(result);
+		}
+	}).catch((err) => {
+		alert('Ошибка запроса на регистрацию');
+		console.log(err);
+	});
 }
 
 function createRoom() {
 	if (socket.connected) {
-		socket.emit('CREATE_ROOM');
+		socket.emit(CREATE_ROOM);
+	} else {
+		alert('Socket disconnected!');
+	}
+}
+
+function joinRoom() {
+	const roomId = inputRoomId.value;
+	console.log('JOIN_ROOM', roomId);
+
+	if (socket && socket.connected && roomId) {
+		socket.emit(JOIN_ROOM, { roomId });
+	} else {
+		alert('Invalid connection or roomId');
 	}
 }
 
 function onRoomCreated(data) {
-	console.log('CREATING_ROOM_ERROR', data);
-}
-
-function onRoomCreatedError(data) {
-	console.log('CREATING_ROOM_ERROR', data);
-}
-
-function joinRoom() {
-	const { userId, roomId } = {};
-	console.log('JOIN_ROOM', { userId, roomId });
-	socket.emit('JOIN_ROOM', { userId, roomId });
+	console.log(ROOM_CREATED, data);
 }
 
 function onRoomJoin(data) {
-	console.log('JOINED_TO_ROOM', data);
+	console.log(JOINED_TO_ROOM, data);
 }
 
 function onUserJoin(data) {
-	console.log('USER_JOIN', data);
+	console.log(USER_JOINED, data);
 }
 
-function onRoomJoinError(data) {
-	console.log('ROOM_JOIN_ERROR', data);
+function initSocket({jwtToken}) {
+	const socket = io(BASE_URL, {
+		auth: {
+			token: jwtToken
+		}
+	});
+
+	socket.on('connect', () => {
+		console.log('Connected');
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Disconnected');
+	});
+
+	socket.on('exception', (err) => {
+		console.log('exception', err);
+		alert('WS Exception');
+	});
+
+	socket.on(ROOM_CREATED, onRoomCreated);
+	socket.on(JOINED_TO_ROOM, onRoomJoin);
+	socket.on(USER_JOINED, onUserJoin);
+
+	return socket;
 }
 
-
-btnConnect.addEventListener('click', function onClickConnect(e) {
-	if (socket.connected && roomId) {
-		console.log('joinRoom', roomId);
-		
-	}
-});
-
-// Default socket events
-
-socket.on('connect', () => {
-	console.log('Connected');
-});
-
-socket.on('disconnect', () => {
-	console.log('Disconnected');
-});
-
-// Room socket events
-
-socket.on('CREATING_ROOM_ERROR', (data) => {
-	console.log('CREATING_ROOM_ERROR', data);
-});
-
-socket.on('ROOM_CREATED', (data) => {
-	console.log('rROOM_CREATED', data);
-});
-
-socket.on('ROOM_JOIN', (data) => {
-	console.log('ROOM_JOIN', data);
-});
-
-socket.on('EXCEPTION', (err) => {
-	console.log('EXCEPTION', err);
-});
+// @ts-ignore
+document.addEventListener('DOMContentLoaded', async () => {
+	inputRegister = document.getElementById('inputRegister');
+	inputRoomId = document.getElementById('inputRoomId');
 });
