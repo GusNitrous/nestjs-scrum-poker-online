94dccf3d8dccae900b6c79b48fa55e82ae979134
GusNitrous <gusnitrous@gmail.com>
2021-11-15 22:27:13 +0300

    Added: tests events

--- src/voting/voting-room/events.ts
+++ src/voting/voting-room/events.ts
@@ -2,6 +2,14 @@ export const CREATE_ROOM = "CREATE_ROOM";
 
 export const ROOM_CREATED = "ROOM_CREATED";
 
-export const JOIN_ROOM = "JOIN_ROOM";
+export const JOIN_USER = "JOIN_USER";
+
+export const USER_JOINED = "USER_JOINED";
+
+export const SEND_MESSAGE = "SEND_MESSAGE";
+
+export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
+
+export const USER_JOINED_TO_ROOM = "USER_JOINED_TO_ROOM";
 
 export const DELETE_ROOM = "DELETE_ROOM";
