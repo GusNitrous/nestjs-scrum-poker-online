74d746e2635e7ed76a3b7e231c91c0585eb72bb0
GusNitrous <gusnitrous@gmail.com>
2022-01-10 21:45:11 +0300

    Cleanup fake client

--- client/utils.js
+++
@@ -1,8 +0,0 @@
-function saveAuthData(data) {
-	sessionStorage.setItem('auth', JSON.stringify(data));
-}
-
-function getAuthData() {
-	const authData = sessionStorage.getItem('auth');
-	return !authData ? null : JSON.parse(authData);
-}
