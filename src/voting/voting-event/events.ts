ba9a5e4629b33240cad85850577cb62484d00e9c
GusNitrous <gusnitrous@gmail.com>
2022-01-17 23:03:12 +0300

    Added: base voting events

---
+++ src/voting/voting-event/events.ts
@@ -0,0 +1,8 @@
+export const VOTING_START  = 'VOTING_START';
+
+export const VOTING_STOP  = 'VOTING_STOP';
+
+export const SEND_SCORE  = 'SEND_SCORE';
+
+export const SHOW_RESULTS  = 'SHOW_RESULTS';
+
