e388588327444fbd3adfbffa77af7dca5f8ad49c
GusNitrous <gusnitrous@gmail.com>
2022-01-27 22:57:08 +0300

    Refactoring: string type for the score value

--- src/room/score.ts
+++ src/room/score.ts
@@ -3,7 +3,7 @@ import { UserId } from '../user/persistence/user.entity';
 export class Score {
     constructor(
         public userId: UserId,
-        public value: number,
+        public value: string,
     ) {
     }
 }
