10eeab898fe08e5038ec7cb0f6896defbed08200
GusNitrous <gusnitrous@gmail.com>
2022-01-31 21:59:00 +0300

    Formatting

--- src/voting/voting-room/events.ts
+++ src/voting/voting-room/events.ts
@@ -1,15 +1,15 @@
-export const CREATE_ROOM = "CREATE_ROOM";
+export const CREATE_ROOM = 'CREATE_ROOM';
 
-export const ROOM_CREATED = "ROOM_CREATED";
+export const ROOM_CREATED = 'ROOM_CREATED';
 
-export const JOIN_USER = "JOIN_USER";
+export const JOIN_USER = 'JOIN_USER';
 
-export const USER_JOINED = "USER_JOINED";
+export const USER_JOINED = 'USER_JOINED';
 
-export const SEND_MESSAGE = "SEND_MESSAGE";
+export const SEND_MESSAGE = 'SEND_MESSAGE';
 
-export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
+export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
 
-export const USER_JOINED_TO_ROOM = "USER_JOINED_TO_ROOM";
+export const USER_JOINED_TO_ROOM = 'USER_JOINED_TO_ROOM';
 
-export const DELETE_ROOM = "DELETE_ROOM";
+export const DELETE_ROOM = 'DELETE_ROOM';
