d1436ee22d578aafc65a84fc6b73f1252d5ed3a8
GusNitrous <gusnitrous@gmail.com>
2022-05-18 12:09:29 +0300

    Fixed: AppController test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,8 +1,9 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import config from './config';
 
-describe("AppController", () => {
+describe('AppController', () => {
     let appController: AppController;
 
     beforeEach(async () => {
@@ -14,9 +15,9 @@ describe("AppController", () => {
         appController = app.get<AppController>(AppController);
     });
 
-    describe("root", () => {
-        it('should return "Hello World!"', () => {
-            expect(appController.getHello()).toBe("Hello World!");
+    describe('ping', () => {
+        it('should return the AppName value', () => {
+            expect(appController.ping()).toBe(config.appName);
         });
     });
 });
