ebe8bc965d294c36e80dac08c0c8439af96fd02a
GusNitrous <gusnitrous@gmail.com>
2022-05-18 12:10:46 +0300

    Added: the "IsLogin" decorator tests

---
+++ src/auth/dto/auth-input.dto.spec.ts
@@ -0,0 +1,51 @@
+import { validateSync } from 'class-validator';
+import { AuthInputDto } from './auth-input.dto';
+
+
+describe('AuthInputDto', () => {
+    let authInputDto: AuthInputDto;
+
+    const validLoginValues = [
+        'Scrummarly',
+        'SM444',
+        'SM7',
+        'G13',
+        'G123',
+        'g123',
+        'Tester',
+        'john',
+        'john15',
+        'Chuck',
+    ];
+
+    const invalidLoginValues = [
+        '13testtest',
+        '!@23',
+        '$23',
+        '^23',
+        '&I nnim',
+        'jo',
+        '$99<.',
+        '_&',
+        '123',
+    ];
+
+    beforeEach(() => {
+        authInputDto = new AuthInputDto();
+    });
+
+    describe('Check "IsLogin" decorator', () => {
+        test.each(validLoginValues)('should return empty errors array on valid login values', (loginValue) => {
+            authInputDto.userName = loginValue;
+            const errors = validateSync(authInputDto);
+            expect(errors.length).toEqual(0);
+        });
+
+        test.each(invalidLoginValues)('should return filled errors array on invalid login values',
+            (loginValue) => {
+                authInputDto.userName = loginValue;
+                const errors = validateSync(authInputDto);
+                expect(errors.length).toEqual(1);
+            });
+    });
+});
