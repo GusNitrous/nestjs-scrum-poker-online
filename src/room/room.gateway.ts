58b91ab3f1eb9dbcfa1c34ae9e1d93f0b1f0af54
GusNitrous <gusnitrous@gmail.com>
2022-05-27 07:24:18 +0300

    Refactoring: moved VotingRoomGateway -> RoomGateway

--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -1,9 +1,7 @@
 import { Logger, UseGuards } from '@nestjs/common';
+import { RoomService } from './room.service';
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
-import { JwtWsGuard } from '../../auth/guards/jwt-ws.guard';
-import { User } from '../../user/persistence/user.entity';
-import { UserService } from '../../user/user.service';
+import { JwtWsGuard } from '../auth/guards/jwt-ws.guard';
 import {
     CREATE_ROOM,
     JOIN_USER,
@@ -12,42 +10,44 @@ import {
     USER_JOINED,
     USER_JOINED_TO_ROOM,
     USER_LEAVE,
-} from './events';
-import { WsCurrentUser } from '../../common/ws/ws-param-decorators';
-import { RoomService } from '../../room/room.service';
+} from './ws-events';
+import { WsCurrentUser } from '../common/ws/ws-param-decorators';
+import { User } from '../user/persistence/user.entity';
+import { Server, Socket } from 'socket.io';
 import { RoomDto } from './dto/room.dto';
 import { OnEvent } from '@nestjs/event-emitter';
+import { AuthEvent } from '../common/app-events/auth-event';
 
-// Room Gateway
 @WebSocketGateway({
     cors: {
         origin: '*',
         methods: ['GET', 'POST'],
     },
 })
-export class VotingRoomGateway {
-    private readonly logger = new Logger(VotingRoomGateway.name);
+@UseGuards(JwtWsGuard)
+export class RoomGateway {
+    private readonly logger = new Logger(RoomGateway.name);
 
     @WebSocketServer()
     private readonly server: Server;
 
     constructor(
-        private readonly userService: UserService,
-        private readonly roomStateService: RoomService,
+        private readonly roomService: RoomService,
     ) {
     }
 
-    @UseGuards(JwtWsGuard)
     @SubscribeMessage(CREATE_ROOM)
     async create(@WsCurrentUser() currentUser: User, @ConnectedSocket() socket: Socket): Promise<void> {
         this.logger.debug('REQUEST_ON_CREATING_ROOM => ' + currentUser);
-        const roomState = await this.roomStateService.createByOwner(currentUser);
-        roomState.addUser(currentUser).startVoting();
-        socket.join(roomState.id);
-        this.server.to(socket.id).emit(ROOM_CREATED, roomState.id);
+        const room = (await this.roomService
+            .createByOwner(currentUser))
+            .addUser(currentUser)
+            .startVoting();
+
+        socket.join(room.id);
+        this.server.to(socket.id).emit(ROOM_CREATED, room.id);
     }
 
-    @UseGuards(JwtWsGuard)
     @SubscribeMessage(JOIN_USER)
     async join(
         @WsCurrentUser() currentUser: User,
@@ -55,15 +55,14 @@ export class VotingRoomGateway {
         @MessageBody('roomId') roomId: string,
     ): Promise<void> {
         this.logger.debug(`REQUEST_ON_JOIN_USER => ${roomId}`);
-        const roomState = await this.roomStateService.getById(roomId);
-        const roomDto = RoomDto.from(roomState.addUser(currentUser));
+        const room = (await this.roomService.findById(roomId)).addUser(currentUser);
+        const roomDto = RoomDto.from(room);
 
         socket.join(roomId);
         socket.to(roomId).emit(USER_JOINED_TO_ROOM, roomDto);
         this.server.to(socket.id).emit(USER_JOINED, roomDto);
     }
 
-    @UseGuards(JwtWsGuard)
     @SubscribeMessage(LEAVE_ROOM)
     async leave(
         @WsCurrentUser() currentUser: User,
@@ -71,15 +70,15 @@ export class VotingRoomGateway {
         @MessageBody('roomId') roomId: string,
     ): Promise<void> {
         this.logger.debug(`REQUEST_ON_LEAVE_FROM_ROOM => ${roomId}`);
-        await this.roomStateService.removeUserFromRooms(currentUser);
+        await this.roomService.removeUserFromRooms(currentUser);
         socket.leave(roomId);
         this.server.emit(USER_LEAVE, currentUser.id);
     }
 
-    @OnEvent('user.logout')
+    @OnEvent(AuthEvent.LOGOUT)
     async userLeave(user: User): Promise<void> {
         this.logger.debug(`${USER_LEAVE} => ` + user);
-        await this.roomStateService.removeUserFromRooms(user);
+        await this.roomService.removeUserFromRooms(user);
         this.server.emit(USER_LEAVE, user.id);
     }
 
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RoomRepository } from './persistence/room.repository';
 import { RoomService } from './room.service';
+import { RoomGateway } from './room.gateway';
+import { UserModule } from '../user/user.module';
 
 @Module({
-    providers: [RoomRepository, RoomService],
+    imports: [UserModule],
+    providers: [RoomRepository, RoomService, RoomGateway],
     exports: [RoomService],
 })
 export class RoomModule {
