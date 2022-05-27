0af73212bb0cc8cf6581fa84fac5e29d64d0e70c
GusNitrous <gusnitrous@gmail.com>
2022-05-27 07:19:44 +0300

    Refactoring: using app config in JwtWsGuard; renamed verifyClient -> verifyWsClient

--- src/auth/guards/jwt-ws.guard.ts
+++ src/auth/guards/jwt-ws.guard.ts
@@ -6,7 +6,6 @@ import {
     NotFoundException,
     UnauthorizedException,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { WsException } from '@nestjs/websockets';
 import * as jwt from 'jsonwebtoken';
 import { VerifyErrors } from 'jsonwebtoken';
@@ -14,19 +13,19 @@ import { User, UserId } from '../../user/persistence/user.entity';
 import { UserService } from '../../user/user.service';
 import { JwtUserPayload } from '../jwt.types';
 import { WsClient } from '../../common/ws/ws-types';
+import config from '../../config';
 
 @Injectable()
 export class JwtWsGuard implements CanActivate {
     private readonly logger = new Logger(JwtWsGuard.name);
 
-    constructor(private readonly config: ConfigService, private readonly userService: UserService) {
+    constructor(private readonly userService: UserService) {
     }
 
     async canActivate(ctx: ExecutionContext): Promise<boolean> {
         const wsClient = ctx.switchToWs().getClient<WsClient>();
         try {
-            const jwtSecret = this.config.get<string>('JWT_SECRET');
-            const payload = await this.verifyClient(wsClient, jwtSecret);
+            const payload = await this.verifyWsClient(wsClient, config.jwtSecret);
             wsClient.data.currentUser = await this.getUserById(payload.id);
         } catch (err) {
             this.logger.error(err?.message || 'WsClient verification error');
@@ -38,12 +37,13 @@ export class JwtWsGuard implements CanActivate {
     private async getUserById(id: UserId): Promise<User> {
         const user = await this.userService.findById(id);
         if (!user) {
+            // TODO remove using NotFoundException?
             throw new WsException(new NotFoundException('User not found'));
         }
         return user;
     }
 
-    private verifyClient(wsClient: WsClient, secret: string): Promise<JwtUserPayload> {
+    private verifyWsClient(wsClient: WsClient, secret: string): Promise<JwtUserPayload> {
         const { auth } = wsClient.handshake;
         return new Promise((resolve, reject) => {
             jwt.verify(auth?.token, secret, (err: VerifyErrors, decoded: JwtUserPayload) => {
