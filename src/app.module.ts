b07dd7ece6b244add6485d0af839f279389fb238
GusNitrous <gusnitrous@gmail.com>
2022-06-03 14:44:06 +0300

    Added: test web-client; using @nestjs/serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { VotingModule } from './modules/voting/voting.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ROOT_DIR } from './config';
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(ROOT_DIR, 'client'),
+        }),
         EventEmitterModule.forRoot({ global: true }),
         AuthModule,
         UserModule,
